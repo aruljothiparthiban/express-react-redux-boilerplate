ae99224f64665c2441230a5a1b6a68c83d4ee932
Aruljothi Parthiban <aruljothiparthiban@hotmail.com>
2020-03-06 00:18:32 +0530

    font awesome added

--- src/client/index.js
+++ src/client/index.js
@@ -3,9 +3,14 @@ import ReactDOM from "react-dom";
 import Header from './components/shared/Header';
 import ContentBody from './components/shared/ContentBody';
 import Footer from './components/shared/Footer';
+import { BrowserRouter as Router } from "react-router-dom";
+
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
 import './assets/scss/dashforge.scss';
 import './assets/scss/pages/dashboard.scss';
-import { BrowserRouter as Router } from "react-router-dom";
 
 class App extends Component {
 
