01d6396c6bd8ae21c63060afe5fba1a367140dfd
Aruljothi Parthiban <aruljothiparthiban@hotmail.com>
2020-03-05 17:54:47 +0530

    header footer change

---
+++ src/client/components/shared/Footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+export default class Footer extends Component {
+
+    render() {
+        return (    
+            <footer className="footer tx-transform-none">
+                <div>
+					<span>© 2020 NGK Dashboard v1.0.0. </span>
+					<span>Developed by <a href="http://www.oriensetech.com/">Oriens eTech Sdn Bhd</a></span>
+				</div>
+            </footer>
+        );
+    }
+}
